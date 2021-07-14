6a1373a15fc74bbbaa669e970ac46ba629bdac16
Gareth <gareth20@gmail.com>
2021-07-14 15:32:47 +1000

    Imported amplify backend and configuration. Building locally. Testing if it will build on Amplify

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import config from '../src/aws-exports';
+import { Amplify } from "aws-amplify";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
+Amplify.configure({
+  ...config, ssr: true
+});
 
 export default MyApp
